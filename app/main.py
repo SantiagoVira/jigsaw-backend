998d62293c97d3e581789f449add36ee4a953241
Aramie Ewen <aramie.ewen@tap.work>
2022-08-01 02:49:08 -0400

    it is because you are amazing and I respect you

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from flask import Flask, request, send_file
 from flask_cors import CORS
-from app.cut_image import shuffle_img
+from app.cut_image import shuffle_img, cut_image
 from PIL import Image
 
 app = Flask(__name__)
@@ -9,15 +9,15 @@ CORS(app)
 
 def serve_pil_image(pil_img):
     img_io = BytesIO()
-    pil_img.save(img_io, 'JPEG', quality=70)
+    pil_img.save(img_io, 'PNG', quality=70)
     img_io.seek(0)
-    return send_file(img_io, mimetype='image/jpeg')
+    return send_file(img_io, mimetype='image/png')
 
 @app.route('/', methods=['GET', 'PUT'])
 def get_data():
     file = Image.open(request.files["image"].stream)
     rows = int(request.form["rows"])
     cols = int(request.form["cols"])
-    final = shuffle_img(file, rows, cols)
+    final = cut_image(file, rows, cols)
     return serve_pil_image(final)
 
